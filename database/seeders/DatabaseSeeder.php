2dd555f453080ee347e4dfa7be086a03334b2abf
djeguede <djeguede.marc.com>
2026-07-25 20:27:18 +0100

    Models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CategorySeeder::class,
+            RolePermissionSeeder::class,
+            UserSeeder::class,
+            CompanySeeder::class
         ]);
+        // \App\Models\User::factory(10)->create();
     }
 }
